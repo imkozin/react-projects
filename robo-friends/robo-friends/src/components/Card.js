055ed649f59340ddba786bcb7a6aed444335d00b
imkozin <ivan.kozin@hotmail.com>
2023-07-17 23:33:32 +0300

    created robots list

---
+++ robo-friends/robo-friends/src/components/Card.js
@@ -0,0 +1,17 @@
+import 'tachyons/css/tachyons.min.css'
+
+const Card = (props) => {
+    const {name, email, id} = props;
+    return (
+        // <h1>RoboFriends</h1>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt=''/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ robo-friends/robo-friends/src/components/CardList.js
@@ -0,0 +1,11 @@
+import Card from './Card';
+
+const CardList = (props) => {
+    const {robots} = props;
+    return (
+        robots.map((user, i) => {
+            return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        }));
+}
+
+export default CardList;
