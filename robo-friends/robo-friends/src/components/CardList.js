186fae1517b3ecd99cde991c3c700f9cb84f13ac
imkozin <ivan.kozin@hotmail.com>
2023-07-23 23:55:02 +0300

    added search and styles

--- robo-friends/robo-friends/src/components/CardList.js
+++ robo-friends/robo-friends/src/components/CardList.js
@@ -4,7 +4,11 @@ const CardList = (props) => {
     const {robots} = props;
     return (
         robots.map((user, i) => {
-            return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+            return <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}/>
         }));
 }
 
